7e964d71114832c807c3d952ec227f2022340798
Nastya <nastya.popper@gmail.com>
2017-08-23 20:08:50 +0300

    Update client.go

--- client.go
+++ client.go
@@ -4,65 +4,64 @@ package main
 import (
 	"fmt"
 	//"io/ioutil"
-	//"log"
+	"log"
 	"net"
-	//"time"
+	//"bufio"
+	"flag"
+	"time"
+	"encoding/json"
 )
-
-/*
-//  Structure for profile of user
-type profile struct {
-
-	id		   int
-	name	   string
-	password   string
-}
-
-*/
-func init() {
-
-	conn, err := net.Dial("tcp", "golang.org:80")
-	if err != nil {
-		// handle error
-	}
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
-
-}
 
 
 func main(){
 
-	/*
-		1. open a telnet connection with hostname and port only
-		2. send a command inside this session or connection
-		3. retrieve the output or http status (OPTIONAL)
-		4. exit/quit session connection
-	 */
-
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		// handle error
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			// handle error
-		}
-		go handleConnection(conn)
-	}
-
-
+	log.Fatal(listenTCP())
 }
 
-func handleConnection (conn *TCPConn) {
+func listenTCP() error {
+
+	var listenAddr string
+	// in command line: -listen-addr = 8080
+	flag.StringVar(&listenAddr, "listen-addr", ":8080", "address to listen")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", listenAddr)
+
+	if err != nil {
+		return fmt.Errorf("error listening on %q: %s", listenAddr, err)
+	}
+	defer func() {
+		closeErr := l.Close()
+		if closeErr != nil {
+			log.Printf("can't close listen socket: %s", err)
+		}
+	}()
+
+	fmt.Printf("Listening on %q\n", listenAddr)
+
+	for {
+		conn, err := l.Accept()
+		//defer conn.Close()
+
+		if err != nil {
+			fmt.Printf("Error accepting connection %q: %s", listenAddr, err)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+
+		go handleConnection(conn)
+
+	}
+}
+
+func handleConnection (conn net.Conn) {
 
 	var i int
 	for {
-		fmt.Println("1. Login")
-		fmt.Println("2. Register")
-		fmt.Println("3. Forgive password")
-		fmt.Println("4. Exit")
+		fmt.Println("1 - Login")
+		fmt.Println("2 - Register")
+		fmt.Println("3 - Forgive password")
+		fmt.Println("4 - Exit")
 
 		fmt.Scanf("%d", &i)
 
@@ -73,20 +72,28 @@ func handleConnection (conn *TCPConn) {
 			register()
 		case 3:
 			fogivePassword()
-		case 4:
-			return
+		case 4: break
+
 		}
 	}
 }
 
 func login() {
+	fmt.Println("login")
+	var data test.Schema
+	if err := readJson(&data); err != nil {
+		log.Fatalf("can't read json data: %s", err)
+	}
+
+	dataToPrint, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Printf("%s", dataToPrint)
 
 }
 
 func  register(){
-
+	fmt.Println("register")
 }
 
 func fogivePassword() {
-
+	fmt.Println("forgive Password")
 }
